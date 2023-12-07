d203d46fd29f07a57579e91356adaf63bedb5f57
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2023-12-07 12:35:42 -0500

    Day 7 - Camel Cards
    Clean up a bunch of redundant logic.

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,24 +1,18 @@
+import kotlin.Comparator
+
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.map(::Hand)
-            .sortedWith(weakToStrongComparator())
-//            .also(::println)
-            .mapIndexed { index, hand -> hand.bid * (index + 1) }
-            .sum()
+        return input.map(::Hand).score()
     }
 
     fun part2(input: List<String>): Int {
-        return input.map { Hand(it, true) }
-            .sortedWith(weakToStrongComparator())
-            .onEach { hand -> println("$hand ${hand.handType}") }
-            .mapIndexed { index, hand -> hand.bid * (index + 1) }
-            .also(::println)
-            .sum()
+        return input.map { Hand(it, true) }.score()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 6440)
+    check(part1(testInput).also(::println) == 6440)
     val testInput2 = readInput("Day07_test")
     check(part2(testInput2).also(::println) == 5905)
 
@@ -27,57 +21,29 @@ fun main() {
     part2(input).println()
 }
 
-data class Hand(val cards: List<PlayingCard>, val bid: Int, val useJokers: Boolean = false) {
-    val handType: HandType = findHandType(useJokers)
+fun List<Hand>.score(): Int = this.sortedWith(weakToStrongComparator())
+    .mapIndexed { index, hand -> hand.bid * (index + 1) }
+    .sum()
 
-    fun findHandType(useJokers: Boolean): HandType {
-        return if (useJokers) {
-            val jokers = cards.count { it == PlayingCard.JOKER }
-            if (jokers == 5) return HandType.FIVE_OF_KIND
-            cards.filterNot { it == PlayingCard.JOKER }
-                .groupBy { it.symbol }
-                .map { (_, list) -> list.size }
-//                .also(::println)
-                .let { instancesOfCards ->
-                    when (instancesOfCards.max() + jokers) {
-                        5 -> HandType.FIVE_OF_KIND
-                        4 -> HandType.FOUR_OF_KIND
-                        3 -> {
-                            if ((instancesOfCards.contains(3) && instancesOfCards.contains(2))
-                                || (instancesOfCards.count { it == 2 } == 2 && jokers == 1)
-                            ) {
-                                HandType.FULL_HOUSE
-                            } else HandType.THREE_OF_KIND
-                        }
+data class Hand(val cards: List<PlayingCard>, val bid: Int) {
+    val handType: HandType = findHandType()
 
-                        2 -> {
-                            if (instancesOfCards.count { it == 2 } == 2) HandType.TWO_PAIR else HandType.PAIR
-                        }
-
-                        else -> HandType.HIGH
-                    }
-
+    private fun findHandType(): HandType {
+        val jokers = cards.count { it == PlayingCard.JOKER }
+        if (jokers == 5) return HandType.FIVE_OF_KIND
+        return cards.filterNot { it == PlayingCard.JOKER }
+            .groupBy { it.symbol }
+            .map { (_, list) -> list.size }
+            .let { instancesOfCards ->
+                when (instancesOfCards.max() + jokers) {
+                    5 -> HandType.FIVE_OF_KIND
+                    4 -> HandType.FOUR_OF_KIND
+                    3 -> if (instancesOfCards.min() == 2) HandType.FULL_HOUSE else HandType.THREE_OF_KIND
+                    2 -> if (instancesOfCards.count { it == 2 } == 2) HandType.TWO_PAIR else HandType.PAIR
+                    else -> HandType.HIGH
                 }
-        } else {
-            cards.groupBy { it.symbol }
-                .map { (_, list) -> list.size }
-//                .also(::println)
-                .let { instancesOfCards ->
-                    when (instancesOfCards.max()) {
-                        5 -> HandType.FIVE_OF_KIND
-                        4 -> HandType.FOUR_OF_KIND
-                        3 -> {
-                            if (instancesOfCards.contains(2)) HandType.FULL_HOUSE else HandType.THREE_OF_KIND
-                        }
 
-                        2 -> {
-                            if (instancesOfCards.count { it == 2 } == 2) HandType.TWO_PAIR else HandType.PAIR
-                        }
-
-                        else -> HandType.HIGH
-                    }
-                }
-        }
+            }
     }
 }
 
@@ -127,19 +93,15 @@ fun fromSymbol(symbol: Char, useJokers: Boolean = false): PlayingCard = when (sy
 
 fun Hand(line: String, useJokers: Boolean = false): Hand {
     val (cards, bid) = line.split(" ")
-    return Hand(cards.map { fromSymbol(it, useJokers) }, bid.toInt(), useJokers)
+    return Hand(cards.map { fromSymbol(it, useJokers) }, bid.toInt())
 }
 
-fun weakToStrongComparator(): Comparator<Hand> {
-    return java.util.Comparator { h1, h2 ->
-        if (h1.handType.ordinal > h2.handType.ordinal) {
-            1
-        } else if (h1.handType.ordinal < h2.handType.ordinal) {
-            -1
-        } else {
-            h1.cards.zip(h2.cards)
-                .map { (c1, c2) -> c1.ordinal - c2.ordinal }
-                .first { it != 0 }
-        }
-    }
+fun handTypeComparator(): Comparator<Hand> = Comparator { h1, h2 -> h1.handType.ordinal - h2.handType.ordinal }
+
+fun cardsComparator(): Comparator<Hand> = Comparator { h1, h2 ->
+    h1.cards.zip(h2.cards)
+        .map { (c1, c2) -> c1.ordinal - c2.ordinal }
+        .first { it != 0 }
 }
+
+fun weakToStrongComparator(): Comparator<Hand> = handTypeComparator().then(cardsComparator())
