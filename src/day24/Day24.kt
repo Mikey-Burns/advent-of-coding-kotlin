e1d0c27bf2e22e0ede88eeffe901cb32224cb815
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2023-12-31 19:24:56 -0500

    Day 24 - Never Tell Me the Odds - Part 2 Partway
    Use velocity offsets to figure out where everything will
    collide, which is the position our rock will start.
    Works for sample, but not real data...

--- src/day24/Day24.kt
+++ src/day24/Day24.kt
@@ -3,9 +3,11 @@ package day24
 import println
 import readInput
 import uniquePairs
+import kotlin.math.abs
 
 private const val LOWER = 200000000000000L
 private const val UPPER = 400000000000000L
+private const val MAX_OFFSET = 500L
 
 fun main() {
     fun part1(input: List<String>, range: LongRange): Int {
@@ -14,22 +16,25 @@ fun main() {
             .count()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>, range: LongRange): Int {
+        return input.map { Linear(it) }
+            .findRockPosition(range)
+            .let { it.x + it.y + it.z }
+            .toInt()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day24_test")
     check(part1(testInput, 7L..27L).also(::println) == 2)
     val testInput2 = readInput("Day24_test")
-    check(part2(testInput2).also(::println) == 0)
+    check(part2(testInput2, 7L..27L).also(::println) == 47)
 
     val input = readInput("Day24")
     part1(input, LOWER..UPPER).println()
-    part2(input).println()
+    part2(input, LOWER..UPPER).println()
 }
 
-private data class Linear(val x: Long, val y: Long, val dx: Long, val dy: Long) {
+private data class Linear(val x: Long, val y: Long, val z: Long, val dx: Long, val dy: Long, val dz: Long) {
     val m: Double = dy.toDouble() / dx
     val b: Double = y - (m * x)
 
@@ -45,14 +50,18 @@ private data class Linear(val x: Long, val y: Long, val dx: Long, val dy: Long) 
         val validY = if (dy > 0) point.second > y else point.second < y
         return validX && validY
     }
+
+    fun predictZ(time: Double, zOffset: Double): Double = (z + time * (dz - zOffset))
 }
 
 private fun Linear(line: String): Linear {
     val (positions, velocities) = line.split("@")
-    val (x, y, _) = positions.split(", ").map { it.trim().toLong() }
-    val (dx, dy, _) = velocities.split(", ").map { it.trim().toLong() }
-    return Linear(x, y, dx, dy)
+    val (x, y, z) = positions.split(", ").map { it.trim().toLong() }
+    val (dx, dy, dz) = velocities.split(", ").map { it.trim().toLong() }
+    return Linear(x, y, z, dx, dy, dz)
 }
+
+private fun Pair<Linear, Linear>.intersectPoint(): Pair<Double, Double>? = first.intersectPoint(second)
 
 private fun Pair<Linear, Linear>.futureIntersectPoint(): Pair<Double, Double>? {
     val intersect = first.intersectPoint(second) ?: return null
@@ -68,3 +77,42 @@ private fun List<Linear>.intersectsInBounds(range: LongRange): List<Pair<Double,
     this.uniquePairs()
         .mapNotNull { it.futureIntersectPoint() }
         .filter { it.isInBounds(range) }
+
+private fun List<Linear>.findRockPosition(range: LongRange): Linear {
+    val xyOffsetCandidates = (0L..MAX_OFFSET)
+        .flatMap { xOffset ->
+            (0L..MAX_OFFSET)
+                .flatMap { yOffset ->
+                    setOf(
+                        xOffset to yOffset,
+                        xOffset to -yOffset,
+                        -xOffset to yOffset,
+                        -xOffset to -yOffset,
+                    )
+                }
+        }
+    val zOffsetCandidates = -MAX_OFFSET..MAX_OFFSET
+
+    return xyOffsetCandidates.firstNotNullOf { (xOffset, yOffset) ->
+        val linears = this.map { it.copy(dx = it.dx - xOffset, dy = it.dy - yOffset) }
+        val intersect = (linears[0] to linears[1]).intersectPoint() ?: return@firstNotNullOf null
+        if (intersect.first % 1.0 != 0.0 || intersect.second % 1.0 != 0.0) return@firstNotNullOf null
+        val target = Linear(intersect.first.toLong(), intersect.second.toLong(), 0, 0, 0, 0)
+        if (linears.any { (it to target).intersectPoint() == null }) return@firstNotNullOf null
+
+
+        val (z, zOffset) = zOffsetCandidates.firstNotNullOfOrNull { zOffset ->
+            val zSet = linears.map {
+                val t = (intersect.first - it.x) / it.dx
+                it.z + (t * (it.dz - zOffset))
+            }
+                .toSet()
+            if (zSet.size == 1) zSet.first() to zOffset else null
+        } ?: return@firstNotNullOf null
+
+
+        Linear(intersect.first.toLong(), intersect.second.toLong(), z.toLong(), xOffset, yOffset, zOffset)
+    }
+}
+
+infix fun Double.eq(other: Double): Boolean = abs(this - other) < 0.00001
