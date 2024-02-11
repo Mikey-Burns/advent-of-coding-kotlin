7e340fafa3494fb6fb85db973a9c9195c17e2d9b
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-02-11 13:45:41 -0500

    2022 Day 8 - Refactored
    Get rid of the custom code per direction.
    Have methods to return the rays of points in each direction,
    then process the lists.

--- src/year2022/day08/Day08.kt
+++ src/year2022/day08/Day08.kt
@@ -4,7 +4,6 @@ import get
 import println
 import readInput
 import utils.Point2D
-import utils.plus
 
 fun main() {
     fun part1(input: List<String>): Long {
@@ -35,90 +34,28 @@ private data class Forest(val grid: List<List<Int>>) {
     }
         .toLong()
 
+    private fun Point2D.isVisible(): Boolean = grid[this].let { height ->
+        listOf(up(), down(), left(), right())
+            .any { list -> list.all { grid[it] < height } }
+    }
+
     fun maxScenicScore(): Long = grid.indices.maxOf { y ->
         grid.first().indices.maxOf { x ->
             Point2D(x, y).scenicScore()
         }
     }
 
-    fun Point2D.isVisible(): Boolean =
-        (visibleFromTop() || visibleFromBottom() || visibleFromLeft() || visibleFromRight())
-
-    fun Point2D.scenicScore(): Long {
-        val scenicUp = scenicUp()
-        val scenicDown = scenicDown()
-        val scenicLeft = scenicLeft()
-        val scenicRight = scenicRight()
-        return scenicUp * scenicDown * scenicLeft * scenicRight
+    private fun Point2D.scenicScore(): Long = grid[this].let { height ->
+        listOf(up(), down(), left(), right())
+            .map { list -> (list.takeWhile { grid[it] < height }.count() + 1).coerceAtMost(list.size) }
+            .reduce { a, b -> a * b }
+            .toLong()
     }
 
-    private fun Point2D.visibleFromTop(): Boolean {
-        if (y == 0) return true
-        val height = grid[this]
-        return (1..y)
-            .map { this + Point2D(0, -it) }
-            .all { grid[it] < height }
-    }
-
-    private fun Point2D.visibleFromBottom(): Boolean {
-        if (y == grid.lastIndex) return true
-        val height = grid[this]
-        return (1..grid.lastIndex - y)
-            .map { this + Point2D(0, it) }
-            .all { grid[it] < height }
-    }
-
-    private fun Point2D.visibleFromLeft(): Boolean {
-        if (x == 0) return true
-        val height = grid[this]
-        return (1..x)
-            .map { this + Point2D(-it, 0) }
-            .all { grid[it] < height }
-    }
-
-    private fun Point2D.visibleFromRight(): Boolean {
-        if (x == grid.first().lastIndex) return true
-        val height = grid[this]
-        return (1..grid.first().lastIndex - x)
-            .map { this + Point2D(it, 0) }
-            .all { grid[it] < height }
-    }
-
-    private fun Point2D.scenicUp(): Long {
-        if (y == 0) return 0L
-        val height = grid[this]
-        val neighbors = (1..y)
-            .map { this + Point2D(0, -it) }
-            .takeWhile { grid[it] < height }
-        return (if (neighbors.size == y) neighbors.size else neighbors.size + 1).toLong()
-    }
-
-    private fun Point2D.scenicDown(): Long {
-        if (y == grid.lastIndex) return 0L
-        val height = grid[this]
-        val neighbors = (1..grid.lastIndex - y)
-            .map { this + Point2D(0, it) }
-            .takeWhile { grid[it] < height }
-        return (if (neighbors.size == grid.lastIndex - y) neighbors.size else neighbors.size + 1).toLong()
-    }
-
-    private fun Point2D.scenicLeft(): Long {
-        if (x == 0) return 0L
-        val height = grid[this]
-        val neighbors =  (1..x)
-            .map { this + Point2D(-it, 0) }
-            .takeWhile { grid[it] < height }
-        return (if (neighbors.size == x) neighbors.size else neighbors.size + 1).toLong()
-    }
-
-    private fun Point2D.scenicRight(): Long {
-        if (x == grid.first().lastIndex) return 0L
-        val height = grid[this]
-        val neighbors =  (1..grid.first().lastIndex - x)
-            .map { this + Point2D(it, 0) }
-            .takeWhile { grid[it] < height }
-        return (if (neighbors.size == grid.first().lastIndex - x) neighbors.size else neighbors.size + 1).toLong()
-    }
+    private fun Point2D.up(): List<Point2D> = (y - 1 downTo 0).map { Point2D(x, it) }
+    private fun Point2D.down(): List<Point2D> = (y + 1..grid.lastIndex).map { Point2D(x, it) }
+    private fun Point2D.left(): List<Point2D> = (x - 1 downTo 0).map { Point2D(it, y) }
+    private fun Point2D.right(): List<Point2D> = (x + 1..grid.first().lastIndex).map { Point2D(it, y) }
 }
 
 private fun List<String>.parseInput(): Forest = Forest(this.map { it.map(Char::digitToInt) })
