4ba4072466e8f714aaa91c4d2382b9af79dbfb12
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-03-02 10:11:52 -0500

    2022 Day 12 - Not Working
    DFS and Dijkstra worked on the sample.
    Need to try BFS

---
+++ src/year2022/day12/Day12.kt
@@ -0,0 +1,99 @@
+package year2022.day12
+
+import get
+import println
+import readInput
+import utils.DijkstraGraph
+import utils.Point2D
+import utils.isInBounds
+import utils.orthogonalNeighbors
+import java.awt.Point
+import kotlin.math.min
+
+fun main() {
+    fun part1(input: List<String>): Long =
+        input.useDijkstra { source, destination ->
+            when {
+                source == 'S' -> destination in 'a'..'b'
+                destination == 'E' -> source == 'z'
+                else -> destination in 'a'..(source + 1)
+            }
+        }
+
+    fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day12_test", "2022")
+    check(part1(testInput).also(::println) == 31L)
+    val testInput2 = readInput("Day12_test", "2022")
+    check(part2(testInput2).also(::println) == 0L)
+
+    val input = readInput("Day12", "2022")
+    part1(input).println()
+    part2(input).println()
+}
+
+private fun List<String>.findPath(validStep: (Char, Char) -> Boolean): Long {
+    val x = this.indexOfFirst { line -> line.contains('S') }
+    val y = this[x].indexOf('S')
+    val start = Point2D(y, x)
+    val points = this.map(String::toList)
+
+    var best = Long.MAX_VALUE
+    val visited = mutableSetOf<Point2D>()
+
+    fun dfs(location: Point2D, steps: Long): Long {
+        val currentChar = points[location]
+        if (currentChar == 'E') {
+            best = min(steps, best)
+            return best
+        }
+        visited += location
+        location.orthogonalNeighbors()
+            .filter { points.isInBounds(it) }
+            .filter { it !in visited }
+            .filter { validStep(currentChar, points[it]) }
+            .forEach { dfs(it, steps + 1) }
+        visited -= location
+        return best
+    }
+
+    return dfs(start, 0L)
+}
+
+private fun List<String>.useDijkstra(validStep: (Char, Char) -> Boolean): Long {
+    val points = this.map(String::toList)
+    val vertices = this.indices.flatMap { y -> this.first().toList().indices.map { x -> Point2D(x, y) } }
+        .toSet()
+    val edges = vertices.associateWith { location ->
+        location.orthogonalNeighbors()
+            .filter { points.isInBounds(it) }
+            .filter { validStep(points[location], points[it]) }
+            .toSet()
+    }
+    val weights = edges.flatMap { (location, destinations) ->
+        destinations.map { location to it }
+    }
+        .associateWith { 1 }
+
+    val graph = DijkstraGraph(vertices, edges, weights)
+    val start = points.find('S')
+    val end = points.find('E')
+    val tree = graph.shortestPathTree(start)
+    val idealPath = graph.idealPath(tree, start, end)
+
+    this.indices.forEach { y -> this.first().toList().indices.map { x -> Point2D(x, y) }
+        .joinToString("") { point -> if (point in idealPath) "X" else " "}
+        .let(::println)
+    }
+
+    return idealPath.size.toLong() - 1L
+}
+
+private fun List<List<Char>>.find(c: Char): Point2D {
+    val y = this.indexOfFirst { line -> line.contains(c) }
+    val x = this[y].indexOf(c)
+    return Point2D(x, y)
+}
