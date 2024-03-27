0e9e2e5d9a9ae26a99eb17f89cfb934bd6ead187
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-03-27 12:01:35 -0400

    2022 Day 15 - Part 2 Hanging
    Part 2 works for the example and hangs forever on the real problem.

--- src/year2022/day15/Day15.kt
+++ src/year2022/day15/Day15.kt
@@ -4,25 +4,25 @@ import println
 import readInput
 import utils.Point2D
 import utils.manhattanDistance
-import utils.plus
 import kotlin.math.abs
 
 fun main() {
-    fun part1(input: List<String>, row: Int): Long = input.toPointsMap().invalidPositionsInRow(row)
+    fun part1(input: List<String>, row: Int): Long = input.toPointsMap().invalidPositionsInRow(row).count().toLong()
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun part2(input: List<String>, maxRow: Int): Long {
+        val beacon = input.toPointsMap().findBeacon(maxRow)
+        return beacon.let { (x, y) -> x * 4000000L + y }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test", "2022")
     check(part1(testInput, 10).also(::println) == 26L)
     val testInput2 = readInput("Day15_test", "2022")
-    check(part2(testInput2).also(::println) == 0L)
+    check(part2(testInput2, 20).also(::println) == 56000011L)
 
     val input = readInput("Day15", "2022")
     part1(input, 2000000).println()
-    part2(input).println()
+    part2(input, 4000000).println()
 }
 
 private fun List<String>.toPointsMap(): Map<Point2D, Point2D> = this.associate { line ->
@@ -33,7 +33,7 @@ private fun List<String>.toPointsMap(): Map<Point2D, Point2D> = this.associate {
     Point2D(sensorX, sensorY) to Point2D(beaconX, beaconY)
 }
 
-private fun Map<Point2D, Point2D>.invalidPositionsInRow(row: Int): Long {
+private fun Map<Point2D, Point2D>.invalidPositionsInRow(row: Int): Set<Int> {
     val beaconX = values.filter { it.y == row }.map { it.x }.toSet()
     val invalidX = entries.flatMap { (sensor, beacon) ->
         val manhattan = sensor.manhattanDistance(beacon)
@@ -43,5 +43,12 @@ private fun Map<Point2D, Point2D>.invalidPositionsInRow(row: Int): Long {
         if (offset >= 0) (sensor.x - offset)..(sensor.x + offset) else emptySet()
     }
         .toSet()
-    return (invalidX - beaconX).count().toLong()
+    return (invalidX - beaconX)
 }
+
+private fun Map<Point2D, Point2D>.findBeacon(maxRow: Int): Point2D = (0..maxRow)
+    .asSequence()
+    .map { row -> row to ((0..maxRow).toSet() - invalidPositionsInRow(row)) }
+    .filterNot { (row, set) -> set.isEmpty() }
+    .map { (row, set) -> Point2D(set.first(), row) }
+    .filterNot { values.contains(it) }.first()
