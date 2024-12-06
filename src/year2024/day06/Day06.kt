1dfd11348cb681a3fffdf3c960065b53b751eaaa
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-06 11:16:05 -0500

    2024 Day 6 - Part 2
    Look at each space in the path (ignoring starting space).
    Walk the path and pretend that space is an obstacle.
    Track the path and the direction we're moving.
    If at any point we repeat a location and direction, we're in a loop,
    so we include that in our count.
    If we get to the edge despite our new obstacle, we do not count it.

--- src/year2024/day06/Day06.kt
+++ src/year2024/day06/Day06.kt
@@ -7,13 +7,13 @@ import year2024.day06.Direction.*
 fun main() {
     fun part1(input: List<String>): Long = tracePath(input).size.toLong()
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long = tryObstacles(input, tracePath(input))
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test", "2024")
     check(part1(testInput) == 41L)
     val testInput2 = readInput("Day06_test", "2024")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 6L)
 
     val input = readInput("Day06", "2024")
     part1(input).println()
@@ -47,6 +47,46 @@ private fun tracePath(input: List<String>): Set<Pair<Int, Int>> {
             visited.add(lineNumber to charNumber)
         }
     }
+}
+
+private fun tryObstacles(input: List<String>, path: Set<Pair<Int, Int>>): Long {
+    // Drop the starting point
+    val startingLine = input.indexOfFirst { it.contains('^') }
+    val startingChar = input[startingLine].indexOf('^')
+    return path.filterNot { it == startingLine to startingChar }
+        .count { (obsLine, obsChar) ->
+            var lineNumber = startingLine
+            var charNumber = startingChar
+            var direction = UP
+            val visited = mutableSetOf(Triple(lineNumber, charNumber, direction))
+
+            while (true) {
+                val candidateLine = when (direction) {
+                    UP -> lineNumber - 1
+                    DOWN -> lineNumber + 1
+                    RIGHT, LEFT -> lineNumber
+                }
+                val candidateChar = when (direction) {
+                    LEFT -> charNumber - 1
+                    RIGHT -> charNumber + 1
+                    UP, DOWN -> charNumber
+                }
+
+                if (candidateLine !in input.indices || candidateChar !in input.first().indices) return@count false
+                if (visited.contains(Triple(candidateLine, candidateChar, direction))) return@count true
+                when {
+                    candidateLine == obsLine && candidateChar == obsChar -> direction++
+                    input[candidateLine][candidateChar] == '#' -> direction++
+                    else -> {
+                        lineNumber = candidateLine
+                        charNumber = candidateChar
+                        visited.add(Triple(lineNumber, charNumber, direction))
+                    }
+                }
+            }
+            error("Cannot reach this")
+        }
+        .toLong()
 }
 
 
