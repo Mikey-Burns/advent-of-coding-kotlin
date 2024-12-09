e9ca0f48f983729adcd210524af8e971c7342341
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-09 11:00:44 -0500

    2024 Day 9 - Part 2
    Instead of a list of Block, use a list of list of Block.
    This allows us to look for empty segments that can fit
    our whole segment of blocks. Eventually we flatten the list
    and reuse our checksum function. Update the checksum function
    to handle Empty Blocks as 0 value instead of throwing.

--- src/year2024/day09/Day09.kt
+++ src/year2024/day09/Day09.kt
@@ -4,24 +4,26 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Long = input.first().toDiskMapList()
+    fun part1(input: List<String>): Long = input.first().toDiskMap()
         .compress()
         .checksum()
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long = input.first().toDiskSegments()
+        .compressSegments()
+        .checksum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test", "2024")
     check(part1(testInput) == 1928L)
     val testInput2 = readInput("Day09_test", "2024")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 2858L)
 
     val input = readInput("Day09", "2024")
     part1(input).println()
     part2(input).println()
 }
 
-private fun String.toDiskMapList(): List<Block> = this.flatMapIndexed { index, c ->
+private fun String.toDiskMap(): List<Block> = this.flatMapIndexed { index, c ->
     val number = c.digitToInt()
     List(number) {
         if (index % 2 == 0) Block.Data(index / 2) else Block.Empty
@@ -36,14 +38,61 @@ private tailrec fun List<Block>.compress(): List<Block> {
     return compressed.dropLast(1).compress()
 }
 
+private fun String.toDiskSegments(): List<List<Block>> = this.mapIndexed { index, c ->
+    val number = c.digitToInt()
+    List(number) {
+        if (index % 2 == 0) Block.Data(index / 2) else Block.Empty
+    }
+}
+
+private fun List<List<Block>>.compressSegments(): List<Block> {
+    val maxId = this.last { it.first() is Block.Data }.first().value
+    val valueToListMap = this
+        .filter { it.isNotEmpty() && it.first() is Block.Data }
+        .associateBy { it.first().value }
+
+    val compressed = this.filter { it.isNotEmpty() }.toMutableList()
+    for (id in maxId downTo 0) {
+        val dataList = valueToListMap[id] ?: continue
+        val idIndex = compressed.indexOf(dataList)
+        val emptyIndex = compressed.indexOfFirst { list -> list.first() is Block.Empty && list.size >= dataList.size }
+        if (emptyIndex == -1 || emptyIndex > idIndex) continue
+        val emptyList = compressed[emptyIndex]
+        when {
+            // Equal sizes, we swap
+            dataList.size == emptyList.size -> {
+                compressed[emptyIndex] = dataList
+                compressed[idIndex] = emptyList
+            }
+            // Otherwise, split the empty list because there is extra empty space
+            else -> {
+                compressed[emptyIndex] = List(emptyList.size - dataList.size) { Block.Empty }
+                compressed[idIndex] = List(dataList.size) { Block.Empty}
+                compressed.add(emptyIndex, dataList)
+            }
+        }
+    }
+
+    return compressed.flatten()
+}
+
 private fun List<Block>.checksum(): Long =
-    this.foldIndexed(0L) { index, total, block -> total + index * (block as Block.Data).id }
+    this.foldIndexed(0L) { index, total, block -> total + index * block.value }
 
 sealed interface Block {
+    val value: Int
+
     data object Empty : Block {
+        override val value: Int
+            get() = 0
+
         override fun toString(): String = "."
     }
+
     data class Data(val id: Int) : Block {
         override fun toString(): String = "$id"
+
+        override val value: Int
+            get() = id
     }
 }
