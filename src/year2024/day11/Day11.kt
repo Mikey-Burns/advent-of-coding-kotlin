a2b03e3782ee4a53d8e6cff285d8832fb6ca6fe4
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-11 09:03:25 -0500

    2024 Day 11 - Part 2
    Inspired by Todd Ginsburg's solution to 2021 Day 6, we do not keep
    track of individual stones. For each iteration, we store a count
    of how many of a given stone number is present in a map.
    We produce a new map each day by shifting stones to their new
    value(s).  Use Map.merge() to set the value to either the count
    or add the count to an existing value.

--- src/year2024/day11/Day11.kt
+++ src/year2024/day11/Day11.kt
@@ -4,15 +4,15 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Long = input.first().toStones().processStones(25).size.toLong()
+    fun part1(input: List<String>): Long = input.first().toStones().processStones(25)
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long = input.first().toStones().processStones(75)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test", "2024")
     check(part1(testInput) == 55312L)
     val testInput2 = readInput("Day11_test", "2024")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 65601038650482L)
 
     val input = readInput("Day11", "2024")
     part1(input).println()
@@ -21,20 +21,29 @@ fun main() {
 
 private fun String.toStones(): List<Long> = this.split(" ").map(String::toLong)
 
-private fun List<Long>.processStones(iterations: Int): List<Long> {
-    var result = this
+private fun List<Long>.processStones(iterations: Int): Long {
+    var stoneMap = this.groupingBy { it }.eachCount().mapValues { (_, count) -> count.toLong() }
     repeat(iterations) {
-        result = result.flatMap { stone ->
-            val stoneString = stone.toString()
-            when {
-                stone == 0L -> listOf(1L)
-                stoneString.length % 2 == 0 -> listOf(
-                    stoneString.substring(0, stoneString.length / 2).toLong(),
-                    stoneString.substring(stoneString.length / 2).toLong()
-                )
-                else -> listOf(stone * 2024)
+        stoneMap = stoneMap.processStones()
+    }
+    return stoneMap.values.sum()
+}
+
+val mergeCount: (t: Long, u: Long) -> Long = { old, new -> old + new }
+
+private fun Map<Long, Long>.processStones(): Map<Long, Long> {
+    val stoneMap = mutableMapOf<Long, Long>()
+    this.forEach { (stone, count) ->
+        val stoneString = stone.toString()
+        when {
+            stone == 0L -> stoneMap.merge(1L, count, mergeCount)
+            stoneString.length % 2 == 0 -> {
+                stoneMap.merge(stoneString.substring(0, stoneString.length / 2).toLong(), count, mergeCount)
+                stoneMap.merge(stoneString.substring(stoneString.length / 2).toLong(), count, mergeCount)
             }
+
+            else -> stoneMap.merge(stone * 2024, count, mergeCount)
         }
     }
-    return result
+    return stoneMap
 }
