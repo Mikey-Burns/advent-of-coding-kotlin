d7a0e9044492680216a17b960ed3c0b4fbe03493
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-13 09:41:52 -0500

    Day 13 - Update
    Clean up the dead code from part 1

--- src/year2024/day13/Day13.kt
+++ src/year2024/day13/Day13.kt
@@ -5,15 +5,13 @@ import readInput
 import kotlin.math.round
 
 fun main() {
-    fun part1(input: List<String>): Long = input.toGames().sumOf(Game::cheapestButtonComboCostViaMath)
+    fun part1(input: List<String>): Long = input.toGames().sumOf(Game::cheapestButtonComboCost)
 
-    fun part2(input: List<String>): Long = input.toBigGames().sumOf(Game::cheapestButtonComboCostViaMath)
+    fun part2(input: List<String>): Long = input.toBigGames().sumOf(Game::cheapestButtonComboCost)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day13_test", "2024")
     check(part1(testInput) == 480L)
-    val testInput2 = readInput("Day13_test", "2024")
-//    check(part2(testInput2) == 0L)
 
     val input = readInput("Day13", "2024")
     part1(input).println()
@@ -52,20 +50,7 @@ private fun toBigGame(aLine: String, bLine: String, prizeLine: String): Game {
 
 private data class Game(val aX: Long, val aY: Long, val bX: Long, val bY: Long, val prizeX: Long, val prizeY: Long) {
 
-    private fun findWinningCombos(): List<Pair<Long, Long>> = buildList {
-        for (a in 0..100L) {
-            for (b in 0..100L) {
-                if ((a * aX + b * bX == prizeX) && (a * aY + b * bY == prizeY)) add(a to b)
-            }
-        }
-    }
-
-    fun cheapestButtonComboCost(): Long = findWinningCombos()
-        .also { if (it.isNotEmpty()) println(it.first()) }
-        .minOfOrNull { (a, b) -> 3L * a + b }
-        ?: 0L
-
-    fun cheapestButtonComboCostViaMath(): Long {
+    fun cheapestButtonComboCost(): Long {
         val b = round((prizeX - (prizeY * aX / aY.toDouble())) / (bX - (bY * aX / aY.toDouble())))
         val a = round((prizeY - (b * bY)) / aY.toDouble())
 
