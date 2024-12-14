c8aa82b081f78b6eca4ecb31f5ede0181acc56e6
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-14 13:55:52 -0500

    Day 14 - Part 2
    Turn each map of robots into a String of X and blank.
    Look for the first iteration where we find a long line of Xs.
    Pray that's correct and print it out.
    Praise your good fortune that it worked.

--- src/year2024/day14/Day14.kt
+++ src/year2024/day14/Day14.kt
@@ -2,33 +2,67 @@ package year2024.day14
 
 import println
 import readInput
+import year2024.day14.Robot.Quadrant.*
 
 fun main() {
     fun part1(input: List<String>, gridX: Int = 101, gridY: Int = 103): Long = input.toRobots(gridX, gridY)
         .map { robot -> robot.patrol(100) }
         .safetyFactor()
 
-    fun part2(input: List<String>, gridX: Int = 101, gridY: Int = 103): Long = 0L
+    fun part2(input: List<String>, gridX: Int = 101, gridY: Int = 103): Long {
+        val candidates = mutableListOf<Int>()
+        var iteration = 0
+        val robots = input.toRobots(gridX, gridY)
+        while (candidates.size < 1) {
+            val patrolled = robots.map { it.patrol(iteration) }
+
+            if (patrolled.toStrings().contains("XXXXXXXXXXXX")) {
+                candidates.add(iteration)
+                println("\n\nIteration: $iteration")
+                patrolled.toStrings().println()
+            }
+            iteration++
+        }
+
+
+        println(candidates)
+
+        return 0L
+    }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14_test", "2024")
     check(part1(testInput, 11, 7) == 12L)
-    val testInput2 = readInput("Day14_test", "2024")
-    check(part2(testInput2) == 0L)
 
     val input = readInput("Day14", "2024")
     part1(input).println()
     part2(input).println()
 }
 
+private fun List<Robot>.toStrings(): String {
+    val map: Map<Pair<Int, Int>, Robot> = this.associateBy { it.x to it.y }
+    return buildList {
+        for (y in 0..103) {
+            add(buildString {
+                for (x in 0..101) {
+                    if (map.containsKey(x to y)) append("X") else append(" ")
+                }
+            })
+        }
+    }
+        .joinToString("\n")
+}
+
 private fun List<String>.toRobots(gridX: Int, gridY: Int): List<Robot> = this.map { Robot(it, gridX, gridY) }
 
-private fun List<Robot>.safetyFactor(): Long = groupingBy { it.getQuadrant() }
-    .eachCount()
+private fun List<Robot>.safetyFactor(): Long = toQuadrants()
     .filterKeys { quadrant -> quadrant != Robot.Quadrant.AXIS }
     .values
     .reduce { a, b -> a * b }
     .toLong()
+
+private fun List<Robot>.toQuadrants(): Map<Robot.Quadrant, Int> = groupingBy { it.getQuadrant() }.eachCount()
 
 private data class Robot(val x: Int, val y: Int, val dx: Int, val dy: Int, val gridX: Int, val gridY: Int) {
 
@@ -45,11 +79,11 @@ private data class Robot(val x: Int, val y: Int, val dx: Int, val dy: Int, val g
         val middleY = (gridY - 1) / 2
 
         return when {
-            x < middleX && y < middleY -> Quadrant.TOP_LEFT
-            x > middleX && y < middleY -> Quadrant.TOP_RIGHT
-            x < middleX && y > middleY -> Quadrant.BOTTOM_LEFT
-            x > middleX && y > middleY -> Quadrant.BOTTOM_RIGHT
-            else -> Quadrant.AXIS
+            x < middleX && y < middleY -> TOP_LEFT
+            x > middleX && y < middleY -> TOP_RIGHT
+            x < middleX && y > middleY -> BOTTOM_LEFT
+            x > middleX && y > middleY -> BOTTOM_RIGHT
+            else -> AXIS
         }
     }
 
