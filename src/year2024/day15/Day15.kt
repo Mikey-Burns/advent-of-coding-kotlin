d563f080cde021d9d2f2c7de358bbaae02c42e15
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-16 10:21:05 -0500

    Day 15 - Part 2
    Create a big warehouse with bigger boxes.
    Store the boxes as objects instead of as locations.
    When we push, we need to look at lists of boxes above us.
    Otherwise, the logic is similar to part 1.

--- src/year2024/day15/Day15.kt
+++ src/year2024/day15/Day15.kt
@@ -14,14 +14,19 @@ fun main() {
         return warehouse.followDirections(directions).boxGps().sum()
     }
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long {
+        val bigWarehouse = input.takeWhile { it.isNotBlank() }.toBigWarehouse()
+        val directions = input.takeLastWhile { it.isNotBlank() }.toDirections()
+
+        return bigWarehouse.followDirections(directions).boxGps().sum()
+    }
 
     // test if implementation meets criteria from the description, like:
     val smallTest = readInput("Day15_test", "2024")
     val bigTest = readInput("Day15_test2", "2024")
     check(part1(smallTest) == 2028L)
     check(part1(bigTest) == 10092L)
-    check(part2(smallTest) == 0L)
+    check(part2(bigTest) == 9021L)
 
     val input = readInput("Day15", "2024")
     part1(input).println()
@@ -60,6 +65,7 @@ private data class Warehouse(var robot: Location, val walls: MutableList<Locatio
         }
     }
 
+    @Suppress("unused")
     private fun printWarehouse() = buildString {
         for (rowNumber in 0..walls.last().first) {
             for (charNumber in 0..walls.last().second) {
@@ -80,6 +86,82 @@ private data class Warehouse(var robot: Location, val walls: MutableList<Locatio
     fun boxGps(): List<Long> = boxes.map { (rowNumber, charNumber) -> rowNumber * 100L + charNumber }
 }
 
+private data class BigWarehouse(var robot: Location, val walls: MutableList<Location>, val boxes: MutableList<BigBox>) {
+    fun followDirections(directions: List<Direction>): BigWarehouse = apply {
+        directions.forEach { direction ->
+            val targetLocation = robot.step(direction)
+            if (targetLocation !in walls && targetLocation !in boxes) {
+                robot = targetLocation
+            } else if (targetLocation in walls) {
+                // No movement
+                Unit
+            } else {
+                // It's a box!
+
+                // Find the box that we are immediately pushing into, add it to a mutable list
+                val boxesToDrop = boxes.filter { it.contains(targetLocation) }.toMutableList()
+                // The next set of boxes to try pushing is initially the box the robot pushes
+                var boxesToPush = boxesToDrop.toSet()
+                var destinationLocations = boxesToPush
+                    // Push the boxes
+                    .map { box -> box.push(direction) }
+                    // Get the locations
+                    .flatMap(BigBox::toLocations)
+                    .toSet()
+                // While there are no walls to be pushed into, we keep trying to push
+                while (destinationLocations.none { location -> location in walls } && boxesToPush.isNotEmpty()) {
+                    // Find boxes we are pushing into
+                    boxesToPush =
+                        boxes.filter { box -> box.left in destinationLocations || box.right in destinationLocations }
+                            // and filter out any we've already pushed
+                            .filter { box -> box !in boxesToDrop }
+                            .toSet()
+                            .also { boxesToDrop.addAll(it) }
+                    destinationLocations = boxesToPush
+                        // Push the boxes
+                        .map { box -> box.push(direction) }
+                        // Get the locations
+                        .flatMap(BigBox::toLocations)
+                        .toSet()
+                }
+
+                // If we ran into a wall after all, we don't push anything
+                if (destinationLocations.any { location -> location in walls }) {
+                    Unit
+                } else {
+                    boxes.removeAll(boxesToDrop)
+                    boxes.addAll(boxesToDrop.map { box -> box.push(direction) })
+                    robot = targetLocation
+                }
+            }
+
+//            printWarehouse()
+        }
+    }
+
+    fun boxGps(): List<Long> = boxes.map { box -> box.left }
+        .map { (rowNumber, charNumber) -> rowNumber * 100L + charNumber }
+
+    @Suppress("unused")
+    fun printWarehouse() = buildString {
+        for (rowNumber in 0..walls.last().first) {
+            for (charNumber in 0..walls.last().second) {
+                val location = rowNumber to charNumber
+                when {
+                    location in walls -> append("#")
+                    boxes.any { bigBox -> bigBox.left == location } -> append("[")
+                    boxes.any { bigBox -> bigBox.right == location } -> append("]")
+                    location == robot -> append("@")
+                    else -> append(".")
+                }
+            }
+            append("\n")
+        }
+        append("\n\n")
+    }
+        .println()
+}
+
 private fun List<String>.toWarehouse(): Warehouse {
     var robot: Location? = null
     val walls = mutableListOf<Location>()
@@ -96,7 +178,62 @@ private fun List<String>.toWarehouse(): Warehouse {
     return Warehouse(robot ?: error("No robot found"), walls, boxes)
 }
 
+private fun List<String>.toBigWarehouse(): BigWarehouse {
+    var robot: Location? = null
+    val walls = mutableListOf<Location>()
+    val boxes = mutableListOf<BigBox>()
+    this.forEachIndexed { rowNumber, row ->
+        row.forEachIndexed { charNumber, c ->
+            when (c) {
+                '@' -> robot = rowNumber to charNumber * 2
+                '#' -> {
+                    walls.add(rowNumber to charNumber * 2)
+                    walls.add(rowNumber to charNumber * 2 + 1)
+                }
+
+                'O' -> {
+                    boxes.add(
+                        BigBox(
+                            rowNumber to charNumber * 2,
+                            rowNumber to charNumber * 2 + 1
+                        )
+                    )
+                }
+            }
+        }
+    }
+    return BigWarehouse(robot ?: error("No robot found"), walls, boxes)
+}
+
 private fun List<String>.toDirections(): List<Direction> = this.joinToString("").map(Direction::fromChar)
+
+private data class BigBox(val left: Location, val right: Location) {
+
+    fun contains(location: Location): Boolean = location == left || location == right
+
+    fun push(direction: Direction): BigBox {
+        return when (direction) {
+            UP -> BigBox(up())
+            RIGHT -> BigBox(right())
+            DOWN -> BigBox(down())
+            LEFT -> BigBox(left())
+        }
+    }
+
+    val toLocations: List<Location>
+        get() = listOf(left, right)
+
+    companion object {
+        fun BigBox(locations: Pair<Location, Location>): BigBox = BigBox(locations.first, locations.second)
+        fun BigBox.up(): Pair<Location, Location> = left.up() to right.up()
+        fun BigBox.down(): Pair<Location, Location> = left.down() to right.down()
+        fun BigBox.left(): Pair<Location, Location> = left.left() to right.left()
+        fun BigBox.right(): Pair<Location, Location> = left.right() to right.right()
+    }
+}
+
+private operator fun List<BigBox>.contains(location: Location): Boolean =
+    this.any { bigBox -> bigBox.contains(location) }
 
 private enum class Direction {
     UP, RIGHT, DOWN, LEFT;
@@ -111,7 +248,6 @@ private enum class Direction {
         }
     }
 }
-
 
 private fun Location.step(direction: Direction): Location = when (direction) {
     UP -> up()
