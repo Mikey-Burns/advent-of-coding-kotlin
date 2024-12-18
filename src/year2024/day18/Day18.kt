1e1927df60c9a1ce2081710a5f85083eab679fbc
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-18 14:46:25 -0500

    Day 18 - Part 2
    Initially used brute force to run Dijkstra with every number
    of rocks until we hit a failure. That worked but was slow.
    Changed the findFirstBlocker method to binary search for
    the first failing number of obstacles.
    Remember to offset by one to convert from the failing number
    of rocks to the index of the failing rock.

--- src/year2024/day18/Day18.kt
+++ src/year2024/day18/Day18.kt
@@ -8,14 +8,15 @@ import kotlin.math.min
 fun main() {
     fun part1(input: List<String>, gridSize: Int = 70, numObstacles: Int = 1024): Long =
         pathWithDijkstra(input.placeObstacles(numObstacles), gridSize)
+            .getValue(gridSize to gridSize).toLong()
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>, gridSize: Int = 70): String = findFirstBlocker(input, gridSize)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day18_test", "2024")
     check(part1(testInput, 6, 12) == 22L)
     val testInput2 = readInput("Day18_test", "2024")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2, 6) == "6,1")
 
     val input = readInput("Day18", "2024")
     part1(input).println()
@@ -26,7 +27,7 @@ private fun List<String>.placeObstacles(numberOfObstacles: Int): List<Location> 
     .map { line -> line.split(",").map { it.toInt() } }
     .map { (x, y) -> Location(x, y) }
 
-private fun pathWithDijkstra(obstacles: List<Location>, gridSize: Int): Long {
+private fun pathWithDijkstra(obstacles: List<Location>, gridSize: Int): Map<Pair<Int, Int>, Int> {
     val range = 0..gridSize
     val unvisited = buildSet {
         for (x in 0..gridSize) {
@@ -40,6 +41,9 @@ private fun pathWithDijkstra(obstacles: List<Location>, gridSize: Int): Long {
         .apply { this[0 to 0] = 0 }
     while (unvisited.isNotEmpty()) {
         val currentNode = unvisited.minBy { distances.getValue(it) }
+        unvisited.remove(currentNode)
+        val currentDistance = distances.getValue(currentNode)
+        if (currentDistance == Int.MAX_VALUE) break
         with(currentNode) {
             listOf(left(), right(), up(), down())
                 .filter { neighbor -> neighbor.first in range && neighbor.second in range }
@@ -48,8 +52,25 @@ private fun pathWithDijkstra(obstacles: List<Location>, gridSize: Int): Long {
                     distances[neighbor] = min(distances.getValue(currentNode) + 1, distances.getValue(neighbor))
                 }
         }
-        unvisited.remove(currentNode)
     }
 
-    return distances.getValue(gridSize to gridSize).toLong()
+    return distances
+}
+
+private fun findFirstBlocker(input: List<String>, gridSize: Int): String {
+    var success = 0
+    var failure = input.lastIndex
+
+    while (success + 1 < failure) {
+        val candidate = (success + failure) / 2
+        val obstacles = input.placeObstacles(candidate)
+        val distances = pathWithDijkstra(obstacles, gridSize)
+        if (distances.getValue(gridSize to gridSize) != Int.MAX_VALUE) {
+            success = candidate
+        } else {
+            failure = candidate
+        }
+    }
+    // Off by 1 because failure is the number of obstacles, and we need the 0-based index of that line
+    return input[failure - 1]
 }
