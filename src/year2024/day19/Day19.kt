17ede694fb46c5d2e96d8c698a79aa476a01f9c6
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-19 10:06:15 -0500

    Day 19 - Part 2
    Instead of a cache of true/false, cache how many possibilities
    there are.  We have very similar code that can be consolidated.

--- src/year2024/day19/Day19.kt
+++ src/year2024/day19/Day19.kt
@@ -7,16 +7,20 @@ fun main() {
     fun part1(input: List<String>): Long {
         val towels = getTowels(input)
         val combos = getCombos(input)
-        return combos.countPossibilities(towels)
+        return combos.countIsPossible(towels)
     }
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long {
+        val towels = getTowels(input)
+        val combos = getCombos(input)
+        return combos.countAllPossibilities(towels)
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day19_test", "2024")
     check(part1(testInput) == 6L)
     val testInput2 = readInput("Day19_test", "2024")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 16L)
 
     val input = readInput("Day19", "2024")
     part1(input).println()
@@ -27,7 +31,7 @@ private fun getTowels(input: List<String>): List<String> = input.first().split("
 
 private fun getCombos(input: List<String>): List<String> = input.takeLastWhile { it.isNotBlank() }
 
-private fun List<String>.countPossibilities(towels: List<String>): Long {
+private fun List<String>.countIsPossible(towels: List<String>): Long {
     val cache: MutableMap<String, Boolean> = towels.associateWith { true }.toMutableMap()
     fun String.isPossible(towels: List<String>): Boolean {
         if (this in cache) return cache.getValue(this)
@@ -40,7 +44,19 @@ private fun List<String>.countPossibilities(towels: List<String>): Long {
             }
     }
 
-    return this.count { combo -> combo.isPossible(towels)
-//        .also { possible -> println("Combo: '$combo' is ${if(possible) "possible" else "not possible"}") }
-    }.toLong()
+    return this.count { combo -> combo.isPossible(towels) }.toLong()
+}
+
+private fun List<String>.countAllPossibilities(towels: List<String>): Long {
+    val cache: MutableMap<String, Long> = mutableMapOf()
+
+    fun String.numberOfCombos(towels: List<String>): Long {
+        if (this in cache) return cache.getValue(this)
+        if (this.isEmpty()) return 1
+        return towels.filter { towel -> this.startsWith(towel) }
+            .sumOf { towel -> this.substringAfter(towel).numberOfCombos(towels) }
+            .also { cache[this] = it }
+    }
+
+    return this.sumOf { combo -> combo.numberOfCombos(towels) }
 }
