505229304cc5b1c8a04ce8653f494b5a4dd650c6
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2024-12-31 17:42:25 -0500

    Day 22 - Part 2
    For each starting secret, we find the 2000 values we will produce.
    Use windowed to go through each set of 5 values (4 changes)
    and if it is the first time we've seen that sequence, add it
    to a running map of sequences to bananas.
    When we have finished every starting secret, we can pick the
    largest number of bananas.

--- src/year2024/day22/Day22.kt
+++ src/year2024/day22/Day22.kt
@@ -7,13 +7,13 @@ fun main() {
     fun part1(input: List<String>): Long = input
         .map { it.toLong() }.sumOf { it.evolveTimes(2000) }
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long = input.bestSequence()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day22_test", "2024")
     check(part1(testInput) == 37327623L)
-    val testInput2 = readInput("Day22_test", "2024")
-    check(part2(testInput2) == 0L)
+    val testInput2 = readInput("Day22_test2", "2024")
+    check(part2(testInput2) == 23L)
 
     val input = readInput("Day22", "2024")
     part1(input).println()
@@ -42,3 +42,28 @@ private fun Long.evolve(): Long {
     result %= 16777216
     return result
 }
+
+private fun List<String>.bestSequence(): Long = this.fold(mutableMapOf<List<Long>, Long>()) { bananas, line ->
+    var secret = line.toLong()
+    val sequences = mutableSetOf<List<Long>>()
+    buildList {
+        add(secret)
+        repeat(2000) {
+            secret = secret.evolve()
+            add(secret)
+        }
+    }
+        .map { it % 10 }
+        .windowed(5)
+        .forEach { secrets ->
+            val sequence = secrets.zipWithNext { a, b -> a - b }
+            if (!sequences.contains(sequence)) {
+                bananas[sequence] = secrets.last() + (bananas[sequence] ?: 0)
+                sequences.add(sequence)
+            }
+        }
+
+    bananas
+}
+    .values
+    .max()
