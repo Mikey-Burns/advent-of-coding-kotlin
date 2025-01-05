99fce41ea3a5107352a5e098c4cd0d75dde08ecc
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-01-05 16:17:29 -0500

    Day 24 - Part 2
    The code is a bit all over the place and requires manual work.
    The first step is to trace back the heritage of the gates to
    the base x and y wires. For every gate, find the heritage
    of the first wire and second wire, alphabetize the results and
    wrap it in parentheses. Recurse this formula and you get a map
    of wire name to gate string. Print out the Z gates and focus
    on the first instance where the actual bit in the result doesn't
    match the expected value. Manually figure out what gates to
    swap to fix that gate, then move on to the next issue.
    Swapping uses some convenient Set operations in a fold.
    As we added more swaps, the problems got pushed to higher Z values
    until addition works.

--- src/year2024/day24/Day24.kt
+++ src/year2024/day24/Day24.kt
@@ -7,15 +7,30 @@ import year2024.day24.Operator.*
 fun main() {
     fun part1(input: List<String>): Long = input.toWireMap().processWires()
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): String = input.toWireMap()
+        .let {
+            val swaps = listOf(
+                "z14" to "hbk",
+                "z18" to "kvn",
+                "z23" to "dbb",
+                "cvh" to "tfn"
+            )
+            it.swapGates(swaps)
+                .also { wireMap ->
+                    wireMap.processWires()
+                    val mappedGates =
+                        wireMap.mapGates().toList().sortedWith(compareBy { (_, value) -> value }).toMap()
+                    wireMap.printXYZ()
+                    println()
+                }
+            return swaps.flatMap(Pair<String, String>::toList).sorted().joinToString(",")
+        }
 
     // test if implementation meets criteria from the description, like:
     val smallInput = readInput("Day24_test", "2024")
     val bigInput = readInput("Day24_test2", "2024")
     check(part1(smallInput) == 4L)
     check(part1(bigInput) == 2024L)
-    val testInput2 = readInput("Day24_test", "2024")
-    check(part2(testInput2) == 0L)
 
     val input = readInput("Day24", "2024")
     part1(input).println()
@@ -50,24 +65,81 @@ private data class WireMap(val gates: Set<Gate>, val initialWires: Map<String, B
         while (processedGates != gates) {
             gates.forEach { gate ->
                 if (wires[gate.first] != null && wires[gate.second] != null) {
-                    wires[gate.destination] = when(gate.operator) {
+                    wires[gate.destination] = when (gate.operator) {
                         AND -> wires.getValue(gate.first)!! && wires.getValue(gate.second)!!
                         OR -> wires.getValue(gate.first)!! || wires.getValue(gate.second)!!
-                        XOR -> wires.getValue(gate.first)!! xor  wires.getValue(gate.second)!!
+                        XOR -> wires.getValue(gate.first)!! xor wires.getValue(gate.second)!!
                     }
                     processedGates.add(gate)
                 }
             }
         }
 
-        return buildString {
-            wires.filterKeys { it.startsWith('z') }
-                .toList()
-                .sortedBy { it.first }
-                .reversed()
-                .forEach { (key, wireState) -> append(if (wireState == true) "1" else "0") }
+        return findForLetter('z').toLong(2)
+    }
+
+    fun mapGates(): Map<String, String> = buildMap {
+        fun mappedHeritage(name: String): String = getOrPut(name) {
+            val myGate = gates.singleOrNull { gate -> gate.destination == name } ?: return name
+            val (firstValue, secondValue) = listOf(mappedHeritage(myGate.first), mappedHeritage(myGate.second)).sorted()
+
+            "($firstValue ${myGate.operator} $secondValue)"
         }
-            .toLong(2)
+
+        wires.keys.forEach { wire ->
+            getOrPut(wire) { traceHeritage(wire) }
+        }
+    }
+
+    fun swapGates(swaps: List<Pair<String, String>>): WireMap {
+        if (swaps.isEmpty()) return this
+        return copy(gates = swaps.fold(gates) { newGates, (d1, d2) ->
+            val g1 = newGates.single { gate -> gate.destination == d1 }
+            val g2 = newGates.single { gate -> gate.destination == d2 }
+            (((newGates - g1) - g2) + g1.copy(destination = d2)) + g2.copy(destination = d1)
+        })
+    }
+
+    private fun findForLetter(letter: Char): String = buildString {
+        wires.filterKeys { it.startsWith(letter) }
+            .toList()
+            .sortedBy { it.first }
+            .reversed()
+            .forEach { (_, wireState) -> append(if (wireState == true) "1" else "0") }
+    }
+
+    fun printXYZ() {
+        val x = findForLetter('x')
+        println("x: $x - ${x.toLong(2)}")
+        val y = findForLetter('y')
+        println("y: $y - ${y.toLong(2)}")
+        val desired = x.toLong(2) + y.toLong(2)
+        println("d: ${desired.toULong().toString(2)} - $desired")
+        val z = findForLetter('z')
+        println("z: $z - ${z.toLong(2)}")
+
+        val expected = desired.toULong().toString(2)
+
+        expected.reversed().zip(z.reversed())
+            .forEachIndexed { index, pair ->
+                val wire = "z${index.toString().padStart(2, '0')}"
+                if (pair.first != pair.second) {
+                    println("$wire does not match")
+                }
+
+                // Find heritage
+
+                val withNames = traceHeritage(wire)
+                println("$wire = $withNames")
+            }
+    }
+
+
+    fun traceHeritage(wire: String): String {
+        val myGate = gates.singleOrNull { gate -> gate.destination == wire } ?: return wire
+        val (firstValue, secondValue) = listOf(traceHeritage(myGate.first), traceHeritage(myGate.second)).sorted()
+
+        return "($firstValue ${myGate.operator} $secondValue)"
     }
 }
 
