f2966e6c7f6d68e6a909519235a73248bcadf765
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-01-13 09:59:01 -0500

    2015 Day 14
    Parse the reindeer to know their names, speed, duration, and rest.
    For every full cycle completed, it travels speed * duration.
    For every partial cycle, it travels speed * time before cycle ends
    or full duration if the cycle ended during a rest.
    For part 2, we run this calculation for all times between 1 and
    the time limit. Check who is winning at each second and give them
    a point. Return the max points at the end.

---
+++ src/year2015/day14/Day14.kt
@@ -0,0 +1,48 @@
+package year2015.day14
+
+import println
+import readInput
+
+fun main() {
+    fun part1(input: List<String>, time: Int = 2503): Int = input.map { Reindeer.of(it) }
+        .maxOf { reindeer -> reindeer.locationAtTime(time) }
+
+    fun part2(input: List<String>, time: Int = 2503): Int = input.map { Reindeer.of(it) }
+        .let { reindeer ->
+            val points = reindeer.associate { it.name to 0 }.toMutableMap()
+            for (currentTime in 1..time) {
+                val currentPositions = reindeer.associateWith { it.locationAtTime(currentTime) }
+                currentPositions.filterValues { position -> position == currentPositions.values.max() }
+                    .keys
+                    .map { it.name }
+                    .forEach { winningReindeer -> points[winningReindeer] = points.getValue(winningReindeer) + 1 }
+            }
+            points.values.max()
+        }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day14_test", "2015")
+    check(part1(testInput, 1000) == 1120)
+    val testInput2 = readInput("Day14_test", "2015")
+    check(part2(testInput2, 1000) == 689)
+
+    val input = readInput("Day14", "2015")
+    part1(input).println()
+    part2(input).println()
+}
+
+private data class Reindeer(val name: String, val speed: Int, val duration: Int, val rest: Int) {
+    private val distanceInLoop = speed * duration
+    private val fullLoop = duration + rest
+
+    fun locationAtTime(time: Int): Int =
+        distanceInLoop * (time / fullLoop) + speed * (time % fullLoop).coerceAtMost(duration)
+
+    companion object {
+        fun of(line: String): Reindeer {
+            val name = line.substringBefore(" ")
+            val (speed, duration, rest) = line.split(" ").mapNotNull { it.toIntOrNull() }
+            return Reindeer(name, speed, duration, rest)
+        }
+    }
+}
