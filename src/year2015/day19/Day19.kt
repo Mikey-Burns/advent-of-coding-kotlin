215252e7e46b0471e94f3752955b0fa01ead2467
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-01-17 14:51:49 -0500

    2015 Day 19 - Part 2 Reddit Magic
    Reddit pointed out some rules for manipulating the string.
    That gets us a quick answer.

--- src/year2015/day19/Day19.kt
+++ src/year2015/day19/Day19.kt
@@ -6,13 +6,14 @@ import readInput
 fun main() {
     fun part1(input: List<String>): Int = input.toChemistryMachine().distinctMolecules()
 
-    fun part2(input: List<String>): Int = 0
+    fun part2(input: List<String>): Int = input.toChemistryMachine().magic()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day19_test", "2015")
     check(part1(testInput) == 4)
     val testInput2 = readInput("Day19_test2", "2015")
-    check(part2(testInput2) == 0)
+    // Add one back because there are direct transitions from e
+    check(part2(testInput2) + 1 == 6)
 
     val input = readInput("Day19", "2015")
     part1(input).println()
@@ -43,5 +44,23 @@ private data class ChemistryMachine(val replacements: List<Pair<String, String>>
         }
 
         return molecules.size
+    }
+
+
+    fun magic(): Int {
+        val subbed = replacements.fold(
+            // Reddit pointed out that Y is always in the middle of other clauses, make it a comma
+            startingMolecule.replace("Y", ",")
+                // Rn always leads before we get to Ar
+                .replace("Rn", "(")
+                // Ar always follows Rn
+                .replace("Ar", ")")
+        ) { s, (key, _) ->
+            // Convert our keys to a single character
+            s.replace(key, "X")
+        }
+        // Length of string - number of wrappers - (2x instances of Y because it takes out 2 characters)
+        // - 1 because the initial step always goes from 1 character to 2
+        return subbed.count() - subbed.count { it in "()" } - (2 * subbed.count { it == ',' }) - 1
     }
 }
