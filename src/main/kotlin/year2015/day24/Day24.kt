6cbee67f8e99fd9d22e3e4c20b97c7223449b9e3
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-01-25 15:30:42 -0500

    2015 Day 24 - Part 2
    Non-generic solution.  Just add an extra layer of nesting
    for 4 groups instead of 3.

--- src/main/kotlin/year2015/day24/Day24.kt
+++ src/main/kotlin/year2015/day24/Day24.kt
@@ -7,13 +7,13 @@ import kotlin.time.measureTime
 fun main() {
     fun part1(input: List<String>): Long = input.map { it.toLong() }.findQuantumEntanglement()
 
-    fun part2(input: List<String>): Int = 0
+    fun part2(input: List<String>): Long = input.map { it.toLong() }.levelFourEntanglement()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day24_test", "2015")
     check(part1(testInput) == 99L)
     val testInput2 = readInput("Day24_test", "2015")
-    check(part2(testInput2) == 0)
+    check(part2(testInput2) == 44L)
 
     val input = readInput("Day24", "2015")
     measureTime { part1(input).println() }.println()
@@ -55,6 +55,52 @@ private fun List<Long>.findQuantumEntanglement(): Long {
 
         if (candidates.isNotEmpty()) {
             return candidates.minOf { it.fold(1L) { left, right -> left * right } }
+        }
+    }
+    error("No valid groups")
+}
+
+private fun List<Long>.levelFourEntanglement(): Long {
+    val total = this.sum()
+    val groupTarget = total / 4
+    val maxGroupSize = this.runningFold(0L) { sum, value -> sum + value}.indexOfFirst { it > groupTarget } + 1
+    for (groupSize in 1..maxGroupSize) {
+        val candidates = mutableListOf<List<Long>>()
+        // Find candidates
+        fun getCombinations(list: List<Long>, size: Int, partialList: List<Long> = emptyList<Long>()): List<List<Long>> {
+            return if (partialList.size == size) {
+                if (partialList.sum() == groupTarget) {
+                    listOf(partialList)
+                } else {
+                    emptyList()
+                }
+            } else {
+                list.flatMapIndexed { index, element ->
+                    getCombinations(list.drop(index + 1), size, partialList + element)
+                }
+            }
+        }
+
+        val firstGroups = getCombinations(this, groupSize)
+        firstGroups.forEach { groupOne ->
+            for (groupTwoSize in 1..(this.size - groupSize)) {
+                val groupTwos = getCombinations(this - groupOne, groupTwoSize)
+                groupTwos.forEach { groupTwo ->
+                    for (groupThreeSize in 1..(this.size - groupSize)) {
+                        val otherGroups = getCombinations(this - groupOne - groupTwo, groupThreeSize)
+                        if (otherGroups.isNotEmpty()) {
+                            // If there is a way to group the remaining numbers, our first group is a candidate
+                            // so we can stop looking at this first group
+                            candidates.add(groupOne)
+                            break
+                        }
+                    }
+                }
+
+                if (candidates.isNotEmpty()) {
+                    return candidates.minOf { it.fold(1L) { left, right -> left * right } }
+                }
+            }
         }
     }
     error("No valid groups")
