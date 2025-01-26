b71a9a6d30b8bc334d734d125559a0892a834bbb
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-01-26 11:00:34 -0500

    Year 2016 - Day 2 Rewrite
    Common helper for moving around an arbitrary numpad.
    Provide specific mapping implementations for each part.

--- src/main/kotlin/year2016/day02/Day02.kt
+++ src/main/kotlin/year2016/day02/Day02.kt
@@ -20,80 +20,107 @@ fun main() {
     measureTime { part2(input).println() }.println()
 }
 
-private fun List<String>.toNumpad(): String {
-    var position = 1 to 1
+/**
+ * Find the code for an arbitrary numpad.
+ *
+ * @param startingPosition Location of the 5 to start at.
+ * @param getButton Mapping function from position to numpad button.
+ * @param nextButton Mapping function to find the next button given a direction to move.
+ */
+private fun List<String>.numpadCode(
+    startingPosition: Pair<Int, Int>,
+    getButton: (Pair<Int, Int>) -> Char,
+    nextButton: (Pair<Int, Int>, Char) -> Pair<Int, Int>
+): String {
+    var position = startingPosition
     var result = ""
     this.forEach { line ->
         line.forEach { direction ->
-            position = when (direction) {
-                'U' -> position.copy(second = (position.second - 1).coerceAtLeast(0))
-                'R' -> position.copy(first = (position.first + 1).coerceAtMost(2))
-                'D' -> position.copy(second = (position.second + 1).coerceAtMost(2))
-                'L' -> position.copy(first = (position.first - 1).coerceAtLeast(0))
-                else -> error("Bad direction!")
-            }
+            position = nextButton(position, direction)
         }
-        result += position.toNumpadButton()
+        result += getButton(position)
     }
     return result
 }
 
-private fun Pair<Int, Int>.toNumpadButton(): Int = when (this) {
-    0 to 0 -> 1
-    1 to 0 -> 2
-    2 to 0 -> 3
-    0 to 1 -> 4
-    1 to 1 -> 5
-    2 to 1 -> 6
-    0 to 2 -> 7
-    1 to 2 -> 8
-    2 to 2 -> 9
+/**
+ * Use the square numpad to find the code.
+ */
+private fun List<String>.toNumpad(): String = numpadCode(
+    1 to 1,
+    ::squareNumpad
+) { position, direction ->
+    when (direction) {
+        'U' -> position.copy(second = (position.second - 1).coerceAtLeast(0))
+        'R' -> position.copy(first = (position.first + 1).coerceAtMost(2))
+        'D' -> position.copy(second = (position.second + 1).coerceAtMost(2))
+        'L' -> position.copy(first = (position.first - 1).coerceAtLeast(0))
+        else -> error("Bad direction!")
+    }
+}
+
+/**
+ * Map a position to the square numpad.
+ * Top left is (0, 0)
+ */
+private fun squareNumpad(position: Pair<Int, Int>): Char = when (position) {
+    0 to 0 -> '1'
+    1 to 0 -> '2'
+    2 to 0 -> '3'
+    0 to 1 -> '4'
+    1 to 1 -> '5'
+    2 to 1 -> '6'
+    0 to 2 -> '7'
+    1 to 2 -> '8'
+    2 to 2 -> '9'
     else -> error("Off the pad!")
 }
 
-private fun List<String>.toFancyNumpad(): String {
-    var position = 0 to 2
-    var result = ""
-    this.forEach { line ->
-        line.forEach { direction ->
-            position = when (direction) {
-                'U' -> when (position.first) {
-                    0, 4 -> position
-                    1, 3 -> position.copy(second = (position.second - 1).coerceIn(1, 3))
-                    2 -> position.copy(second = (position.second - 1).coerceIn(0, 4))
-                    else -> error("Off the grid")
-                }
-
-                'R' -> when (position.second) {
-                    0, 4 -> position
-                    1, 3 -> position.copy(first = (position.first + 1).coerceIn(1, 3))
-                    2 -> position.copy(first = (position.first + 1).coerceIn(0, 4))
-                    else -> error("Off the grid")
-                }
-
-                'D' -> when (position.first) {
-                    0, 4 -> position
-                    1, 3 -> position.copy(second = (position.second + 1).coerceIn(1, 3))
-                    2 -> position.copy(second = (position.second + 1).coerceIn(0, 4))
-                    else -> error("Off the grid")
-                }
-
-                'L' -> when (position.second) {
-                    0, 4 -> position
-                    1, 3 -> position.copy(first = (position.first - 1).coerceIn(1, 3))
-                    2 -> position.copy(first = (position.first - 1).coerceIn(0, 4))
-                    else -> error("Off the grid")
-                }
-
-                else -> error("Bad direction!")
-            }
+/**
+ * Use the fancy numpad to find the code
+ */
+private fun List<String>.toFancyNumpad(): String = numpadCode(
+    0 to 2,
+    ::fancyNumpad
+) { position, direction ->
+    when (direction) {
+        'U' -> when (position.first) {
+            0, 4 -> position
+            1, 3 -> position.copy(second = (position.second - 1).coerceIn(1, 3))
+            2 -> position.copy(second = (position.second - 1).coerceIn(0, 4))
+            else -> error("Off the grid")
         }
-        result += position.toFancyNumpadButton()
+
+        'R' -> when (position.second) {
+            0, 4 -> position
+            1, 3 -> position.copy(first = (position.first + 1).coerceIn(1, 3))
+            2 -> position.copy(first = (position.first + 1).coerceIn(0, 4))
+            else -> error("Off the grid")
+        }
+
+        'D' -> when (position.first) {
+            0, 4 -> position
+            1, 3 -> position.copy(second = (position.second + 1).coerceIn(1, 3))
+            2 -> position.copy(second = (position.second + 1).coerceIn(0, 4))
+            else -> error("Off the grid")
+        }
+
+        'L' -> when (position.second) {
+            0, 4 -> position
+            1, 3 -> position.copy(first = (position.first - 1).coerceIn(1, 3))
+            2 -> position.copy(first = (position.first - 1).coerceIn(0, 4))
+            else -> error("Off the grid")
+        }
+
+        else -> error("Bad direction!")
     }
-    return result
 }
 
-private fun Pair<Int, Int>.toFancyNumpadButton(): Char = when (this) {
+/**
+ * Map a position to the fancy numpad.
+ * Top left is (0, 0)
+ */
+private fun fancyNumpad(position: Pair<Int, Int>): Char = when (position) {
     2 to 0 -> '1'
     1 to 1 -> '2'
     2 to 1 -> '3'
