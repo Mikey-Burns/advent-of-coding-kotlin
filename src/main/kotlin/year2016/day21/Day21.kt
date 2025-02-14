52d7d43c1a60149041a03315cb2a1670d52e308b
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-02-13 20:50:08 -0500

    Year 2016 - Day 21 Algorithm
    Reverse the algorithm for part 2.

--- src/main/kotlin/year2016/day21/Day21.kt
+++ src/main/kotlin/year2016/day21/Day21.kt
@@ -7,7 +7,7 @@ import kotlin.time.measureTime
 fun main() {
     fun part1(input: List<String>, start: String = "abcdefgh"): String = scramble(input, start)
 
-    fun part2(input: List<String>, scrambled: String = "fbgdceah"): String = hacker(input, scrambled)
+    fun part2(input: List<String>, scrambled: String = "fbgdceah"): String = reverseScramble(input, scrambled)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day21_test", "2016")
@@ -94,6 +94,92 @@ private fun scramble(input: List<String>, start: String): String {
     return scrambled.joinToString("")
 }
 
+private fun reverseScramble(input: List<String>, scrambled: String): String {
+    val toUnscramble = scrambled.toMutableList()
+    val newToOld = mapOf(
+        1 to 0,
+        3 to 1,
+        5 to 2,
+        7 to 3,
+        10 % 8 to 4,
+        12 % 8 to 5,
+        14 % 8 to 6,
+        16 % 8 to 7,
+    )
+
+    for (instruction in input.reversed()) {
+        val split = instruction.split(" ")
+        when {
+            instruction.startsWith("swap position") -> {
+                val firstIndex = split[2].toInt()
+                val secondIndex = split[5].toInt()
+                val first = toUnscramble[firstIndex]
+
+                toUnscramble[firstIndex] = toUnscramble[secondIndex]
+                toUnscramble[secondIndex] = first
+            }
+
+            instruction.startsWith("swap letter") -> {
+                val first = split[2].first()
+                val second = split[5].first()
+
+                toUnscramble.indices.forEach { index ->
+                    toUnscramble[index] = when (toUnscramble[index]) {
+                        first -> second
+                        second -> first
+                        else -> toUnscramble[index]
+                    }
+                }
+            }
+
+            instruction.startsWith("rotate left") -> {
+                val steps = split[2].toInt()
+                val toMove = toUnscramble.takeLast(steps)
+                repeat(steps) {
+                    toUnscramble.removeLast()
+                }
+                toUnscramble.addAll(0, toMove)
+            }
+
+            instruction.startsWith("rotate right") -> {
+                val steps = split[2].toInt()
+                val toMove = toUnscramble.take(steps)
+                repeat(steps) {
+                    toUnscramble.removeFirst()
+                }
+                toUnscramble.addAll(toMove)
+            }
+
+            instruction.startsWith("rotate based on") -> {
+                val letter = split[6].first()
+                val index = toUnscramble.indexOf(letter)
+                val originalIndex = newToOld.getValue(index)
+                while (toUnscramble[originalIndex] != letter) {
+                    toUnscramble.add(toUnscramble.removeFirst())
+                }
+            }
+
+            instruction.startsWith("reverse") -> {
+                val first = split[2].toInt()
+                val second = split[4].toInt()
+                val sublist = toUnscramble.subList(first, second + 1).reversed()
+                sublist.forEachIndexed { offset, letter ->
+                    toUnscramble[first + offset] = letter
+                }
+            }
+
+            instruction.startsWith("move") -> {
+                val first = split[5].toInt()
+                val second = split[2].toInt()
+                val letter = toUnscramble.removeAt(first)
+                toUnscramble.add(second, letter)
+            }
+        }
+    }
+    return toUnscramble.joinToString("")
+}
+
+@Suppress("unused")
 private fun hacker(input: List<String>, scrambled: String): String {
     val attemptedPasswords = mutableSetOf<String>()
     while (true) {
