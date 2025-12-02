02588ac7f140e98153e4f42a11e8505fae097e5b
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-12-02 14:16:45 -0500

    Year 2025 - Day 02 - Part 2
    Refactor part 1 to take the logic as a parameter.
    Part 2 we check each ID by chunking it.
    If all chunks are identical, the ID is invalid.
    This handles leftover digits because the chunk won't be equal.
    Iterate on chunks of 1 to half the length of the ID.

--- src/main/kotlin/year2025/day02/Day02.kt
+++ src/main/kotlin/year2025/day02/Day02.kt
@@ -5,16 +5,18 @@ import utils.readInput
 import kotlin.time.measureTime
 
 fun main() {
-    fun part1(input: List<String>): Long = input.first().separateRanges().flatMap { it.invalidIds() }.sum()
+    fun part1(input: List<String>): Long =
+        input.first().separateRanges().flatMap { it.invalidIds(::twoHalfInvalidCheck) }.sum()
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long =
+        input.first().separateRanges().flatMap { it.invalidIds(::allSizesInvalidCheck) }.sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test", "2025")
     check(part1(listOf("11-22")) == 33L)
     check(part1(testInput) == 1227775554L)
     val testInput2 = readInput("Day02_test", "2025")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 4174379265L)
 
     val input = readInput("Day02", "2025")
     measureTime { part1(input).println() }.println()
@@ -27,28 +29,43 @@ private fun String.separateRanges(): List<LongRange> = this.split(",")
         start..end
     }
 
-private fun LongRange.invalidIds(): List<Long> {
+private fun LongRange.invalidIds(findInvalidIds: (LongRange) -> List<Long>): List<Long> {
     val first = this.first.toString()
     val last = this.last.toString()
 
-    return when {
-        first.length == last.length && first.length.isOdd() -> emptyList()
-        first.length == last.length -> {
-            (first.toLong()..last.toLong())
-                .filter { it.isInvalid() }
-        }
-
-        else -> {
-            (this.first..("".padEnd(first.length, '9').toLong())).invalidIds() +
-                    (("1".padEnd(last.length, '0').toLong())..this.last).invalidIds()
-        }
+    return if (first.length != last.length) {
+        (this.first..("".padEnd(first.length, '9').toLong())).invalidIds(findInvalidIds) +
+                (("1".padEnd(last.length, '0').toLong())..this.last).invalidIds(findInvalidIds)
+    } else {
+        findInvalidIds(this)
     }
 }
 
-private fun Long.isInvalid(): Boolean {
+private fun twoHalfInvalidCheck(range: LongRange): List<Long> {
+    val first = range.first.toString()
+    val last = range.last.toString()
+
+    return if (first.length == last.length && first.length.isOdd()) emptyList()
+    else (range.first..range.last)
+        .filter { it.isInvalidHalf() }
+}
+
+private fun Long.isInvalidHalf(): Boolean {
     val asString = this.toString()
     if (asString.length.isOdd()) return false
     return asString.take(asString.length / 2) == asString.takeLast(asString.length / 2)
+}
+
+private fun allSizesInvalidCheck(range: LongRange): List<Long> {
+    val maxChunkSize = range.first.toString().length / 2
+    return range.filter { id ->
+        (1..maxChunkSize)
+            .any { chunkSize ->
+                id.toString().chunked(chunkSize).toSet().size == 1
+            }
+    }
+        .toSet()
+        .toList()
 }
 
 private fun Int.isOdd(): Boolean = this % 2 != 0
