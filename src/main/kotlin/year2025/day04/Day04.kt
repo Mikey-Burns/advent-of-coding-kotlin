8f4bcf20df9ba02a3f5e5184fc0c227bd63f0d60
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-12-04 13:25:39 -0500

    Year 2025 - Day 04 - Part 2
    Rewrite to not use a class.
    Recurse the initial function while tracking which points
    we have already cleared.
    When we get to a point where we can't clean anything,
    we return the list of all cleaned points.

--- src/main/kotlin/year2025/day04/Day04.kt
+++ src/main/kotlin/year2025/day04/Day04.kt
@@ -9,34 +9,39 @@ import utils.readInput
 import kotlin.time.measureTime
 
 fun main() {
-    fun part1(input: List<String>): Int = PaperGrid(input).accessibleRolls()
+    fun part1(input: List<String>): Int = input.accessibleRolls().size
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Int = input.cleanupMaxPaper().size
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test", "2025")
     check(part1(testInput) == 13)
     val testInput2 = readInput("Day04_test", "2025")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 43)
 
     val input = readInput("Day04", "2025")
     measureTime { part1(input).println() }.println()
     measureTime { part2(input).println() }.println()
 }
 
-private data class PaperGrid(val grid: List<List<Char>>) {
-    fun accessibleRolls(): Int {
-        return grid.indices.sumOf { column ->
-            grid[column].indices
-                .map { row -> Point2D(row, column) }
-                .filter { point -> grid[point] == '@' }
-                .count { point ->
-                    point.allNeighbors()
-                        .filter { grid.isInBounds(it) }
-                        .count { grid[it] == '@' } < 4
-                }
-        }
+private fun List<String>.accessibleRolls(cleanedPaper: List<Point2D> = emptyList()): List<Point2D> {
+    val grid = this.map<String, List<Char>>(String::toList)
+    return grid.indices.flatMap { column ->
+        grid[column].indices
+            .map { row -> Point2D(row, column) }
+            .filter { point -> point !in cleanedPaper }
+            .filter { point -> grid[point] == '@' }
+            .filter { point ->
+                point.allNeighbors()
+                    .filter { grid.isInBounds(it) }
+                    .filter { it !in cleanedPaper }
+                    .count { grid[it] == '@' } < 4
+            }
     }
 }
 
-private fun PaperGrid(input: List<String>): PaperGrid = PaperGrid(input.map { line -> line.toList() })
+private fun List<String>.cleanupMaxPaper(cleaned: List<Point2D> = emptyList()): List<Point2D> {
+    val newCleaned = this.accessibleRolls(cleaned)
+    if (newCleaned.isEmpty()) return cleaned
+    return cleanupMaxPaper(newCleaned + cleaned)
+}
