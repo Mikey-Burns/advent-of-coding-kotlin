65cf57dc49bae3549bb176386cfe23db1936d989
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-12-08 09:54:32 -0500

    Year 2025 - Day 08 - Part 2
    As we apply each connection, check if we have a circuit
    containing every point.  When we reach that state,
    multiply the x values of the points in our last connection.

--- src/main/kotlin/year2025/day08/Day08.kt
+++ src/main/kotlin/year2025/day08/Day08.kt
@@ -7,22 +7,22 @@ import kotlin.time.measureTime
 
 fun main() {
     fun part1(input: List<String>, numberOfConnections: Int = 1000): Long = input.toPoints()
-//        .also(::println)
         .calculateDistances()
-//        .also(::println)
         .take(numberOfConnections)
         .findCircuits()
-//        .also(::println)
         .take(3)
         .fold(1L) { acc, circuit -> acc * circuit.size }
 
-    fun part2(input: List<String>): Long = 0L
+    fun part2(input: List<String>): Long = input.toPoints()
+        .calculateDistances()
+        .findLastConnection(input.size)
+        .let { (start, end) -> start.x.toLong() * end.x.toLong() }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test", "2025")
     check(part1(testInput, 10) == 40L)
     val testInput2 = readInput("Day08_test", "2025")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 25272L)
 
     val input = readInput("Day08", "2025")
     measureTime { part1(input).println() }.println()
@@ -45,24 +45,37 @@ private fun List<Point3D>.calculateDistances(): List<DistancePair> {
 
 private fun List<DistancePair>.findCircuits(): List<Circuit> {
     val circuits = mutableListOf<MutableSet<Point3D>>()
+    this.forEach { distancePair -> circuits.updateCircuits(distancePair) }
+    return circuits.sortedByDescending { it.size }
+}
 
-    this.forEach { (start, end) ->
-        val startCircuits = circuits.singleOrNull { circuit -> circuit.contains(start) }
-        val endCircuits = circuits.singleOrNull { circuit -> circuit.contains(end) }
+private fun List<DistancePair>.findLastConnection(numberOfPoints: Int): DistancePair {
+    val circuits = mutableListOf<MutableSet<Point3D>>()
 
-        when {
-            startCircuits == null && endCircuits == null -> circuits.add(mutableSetOf(start, end))
-            startCircuits == null && endCircuits != null -> endCircuits.add(start)
-            startCircuits != null && endCircuits == null -> startCircuits.add(end)
-            startCircuits != null && endCircuits != null -> {
-                circuits.remove(startCircuits)
-                circuits.remove(endCircuits)
-                circuits.add((startCircuits + endCircuits).toMutableSet())
-            }
-        }
+    this.forEach { distancePair ->
+        circuits.updateCircuits(distancePair)
+        if (circuits.any { circuit -> circuit.size == numberOfPoints }) return distancePair
     }
 
-    return circuits.sortedByDescending { it.size }
+    throw IllegalStateException("Never made a full circuit!")
+}
+
+private fun MutableList<MutableSet<Point3D>>.updateCircuits(distancePair: DistancePair) = apply {
+    val start = distancePair.first
+    val end = distancePair.second
+    val startCircuits = this.singleOrNull { circuit -> circuit.contains(start) }
+    val endCircuits = this.singleOrNull { circuit -> circuit.contains(end) }
+
+    when {
+        startCircuits == null && endCircuits == null -> this.add(mutableSetOf(start, end))
+        startCircuits == null && endCircuits != null -> endCircuits.add(start)
+        startCircuits != null && endCircuits == null -> startCircuits.add(end)
+        startCircuits != null && endCircuits != null -> {
+            this.remove(startCircuits)
+            this.remove(endCircuits)
+            this.add((startCircuits + endCircuits).toMutableSet())
+        }
+    }
 }
 
 private typealias DistancePair = Triple<Point3D, Point3D, Long>
