db9bb93785fedfa3b4860855ab84857f079ca32b
Mike Burns <5823252+Mikey-Burns@users.noreply.github.com>
2025-12-12 13:16:53 -0500

    Year 2025 - Day 10 - Part 2
    Use the recursive method with simplification to solve part 2.
    So much credit to https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/.

--- src/main/kotlin/year2025/day10/Day10.kt
+++ src/main/kotlin/year2025/day10/Day10.kt
@@ -7,17 +7,16 @@ import kotlin.time.measureTime
 
 fun main() {
     fun part1(input: List<String>): Int = input.map { Machine(it) }
-        .sumOf { it.minimumPresses(it.indicatorLights) }
+        .sumOf { it.minimumPressesForIndicators(it.indicatorLights) }
 
-    fun part2(input: List<String>): Long = input.map { Machine(it) }
-        .map { it.combinations() }
-        .let { 0L }
+    fun part2(input: List<String>): Int = input.map { Machine(it) }
+        .sumOf { it.minimumPressesForJoltage(it.joltages) }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test", "2025")
     check(part1(testInput) == 7)
     val testInput2 = readInput("Day10_test", "2025")
-    check(part2(testInput2) == 0L)
+    check(part2(testInput2) == 33)
 
     val input = readInput("Day10", "2025")
     measureTime { part1(input).println() }.println()
@@ -32,34 +31,80 @@ private data class Machine(
 
     /**
      * Calculate all combinations of button presses where each button is pressed up to one time.
-     * Use a binary representation of each button being unpressed or pressed,
-     * then calculate how much each counter should be increased based on those button presses.
+     * Use a binary representation of each button being unpressed or pressed.
+     * This is mapped to how many buttons were pressed for convenience.
+     *
+     * Not pressing a button is a valid option!
      */
-    fun combinations(): Map<List<Int>, Int> {
-        val counts = List(joltages.size) { 0 }
-        val costs = mutableMapOf<List<Int>, Int>()
-        (1..<(2.0.pow(buttons.size).toInt()))
+    fun combinationsButtonLists(): Map<List<Int>, Int> {
+        val buttonsToCost = mutableMapOf<List<Int>, Int>()
+        (0..<(2.0.pow(buttons.size).toInt()))
             .map { counter -> counter.toString(2).padStart(buttons.size, '0') }
-            .map { binary ->
-                binary.toList()
-                    .mapIndexedNotNull { index, ch -> if (ch == '1') buttons[index] else null }
-            }
-            .forEach { buttonList ->
-                val myCounts = counts.toMutableList()
-                buttonList.forEach { button -> button.lights.forEach { light -> myCounts[light]++ } }
-                costs.putIfAbsent(myCounts.toList(), buttonList.size)
+            .forEach { binary ->
+                val binaryList = binary.toList().map { ch -> if (ch == '1') 1 else 0 }
+                buttonsToCost.putIfAbsent(binaryList, binaryList.count { it == 1 })
             }
 
-        return costs
+        return buttonsToCost
     }
+
+    /**
+     * Helper to convert from a list of button indices
+     * to a list of times each button was pressed.
+     */
+    private fun List<Int>.buttonIndicesToCounts(): List<Int> = MutableList(joltages.size) { 0 }
+        .apply {
+            this@buttonIndicesToCounts.mapIndexedNotNull { index, binary -> if (binary == 1) buttons[index] else null }
+                .forEach { button -> button.lights.forEach { light -> this[light]++ } }
+        }
 
     /**
      * Find the minimum presses to match the boolean flags
      */
-    fun minimumPresses(onOff: List<Boolean> = indicatorLights): Int {
-        return combinations()
-            .filter { (buttonCounts, cost) -> buttonCounts.map { it % 2 == 1 } == onOff }
-            .minOf { (buttonCounts, cost) -> cost }
+    fun minimumPressesForIndicators(onOff: List<Boolean> = indicatorLights): Int {
+        return combinationsButtonLists()
+            .filter { (buttonIndices, _) -> buttonIndices.buttonIndicesToCounts().map { it % 2 == 1 } == onOff }
+            .minOf { (_, cost) -> cost }
+    }
+
+    /**
+     * Find the minimum number of button presses to get a desired joltage outcome.
+     * At each step, identify which button combinations would produce a new joltage
+     * goal where the values are all even. Divide those joltages by 2 to simplify,
+     * then solve the new problem.  Sum up the minimum solutions for each subproblem,
+     * remembering to multiply by 2 wherever we did our simplification.
+     * Cache subproblems as we go for efficiency.
+     * If we ever have an impossible subproblem, cache Int.MAX_VALUE so it never
+     * gets selected as a minimum path.
+     */
+    fun minimumPressesForJoltage(
+        goal: List<Int>,
+        cache: MutableMap<List<Int>, Int> = mutableMapOf()
+    ): Int {
+        if (goal.all { it == 0 }) return 0
+        val booleanGoal = goal.map { it % 2 == 1 }
+
+        return cache.getOrPut(goal) {
+            combinationsButtonLists()
+                .minOf { (binaryList, cost) ->
+                    val counts = binaryList.buttonIndicesToCounts()
+                    if (counts.map { it % 2 == 1 } != booleanGoal) return@minOf Int.MAX_VALUE
+
+                    // Apply the combination to the goal
+                    val undivided = goal.toMutableList().mapIndexed { index, button -> (button - counts[index]) }
+                    val newGoal = undivided.map { it / 2 }
+                    // If any resulting value is below 0, don't go down this path
+                    if (newGoal.any { it < 0 }) Int.MAX_VALUE
+                    else {
+                        val minimumPresses = minimumPressesForJoltage(newGoal, cache)
+                        if (minimumPresses == Int.MAX_VALUE) {
+                            Int.MAX_VALUE
+                        } else {
+                            cost + 2 * minimumPresses
+                        }
+                    }
+                }
+        }
     }
 }
 
